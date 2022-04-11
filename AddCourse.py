01647d35283759539085f595ce9b5c70330a4bfb
Franco23034565 <franco23034565@gmail.com>
2022-04-11 17:42:09 +0800

    Integer detection is not necessary

--- AddCourse.py
+++ AddCourse.py
@@ -2,16 +2,11 @@ f = open("Course.txt", 'r')
 
 tf = open("AddCourse.sql", 'w')
 
-def isInt(s):
-    try:
-        int(s)
-        return True
-    except:
-        return False
 
 def insert_AllCourse(infoList):
     seq_com = f"insert into allcourse values({infoList[0]}, {infoList[1]}, {infoList[2]}, {infoList[3]}, {infoList[4]}, {infoList[5]}, {infoList[6]}, {infoList[7]});\n"
     return seq_com
+
 
 def insert_CourseTime(CourseID, CTList):
     i=0
@@ -30,20 +25,12 @@ def insert_CourseTime(CourseID, CTList):
 for line in f.readlines():
     courseInfoList = []
     for word in line.split():
-        if (isInt(word)):   #is integer
-            #print(int(word))
-            courseInfoList.append(int(word))
-        else:               #not integer
-            #print('\'' + word + '\'')
-            courseInfoList.append(word)
-    #print(courseInfoList)
+        courseInfoList.append(word)
+
     courseTime = courseInfoList[8:]
-    #print(courseTime)
 
     tf.write(insert_AllCourse(courseInfoList))
     tf.write(insert_CourseTime(courseInfoList[0], courseTime))
-    #print(insert_AllCourse(courseInfoList))
-    #print(insert_CourseTime(courseInfoList[0], courseTime))
 
 tf.close()
 f.close()
