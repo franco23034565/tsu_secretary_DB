34200723e17d3f0c91e2eec44712d7824c97a9b7
Peruschi <96690411+Peruschi@users.noreply.github.com>
2022-04-15 16:23:27 +0800

    Add files via upload

--- updateDB.py
+++ updateDB.py
@@ -1,89 +1,82 @@
-#!/usr/bin/env python3
-# coding=utf-8
-# -*- coding: UTF-8 -*-
-from flask import Flask, request
-import MySQLdb
-
-app = Flask(__name__)
-'''
-    form = """
-    <form method="post" action="/action" >
-        文字輸出欄位：<input name="my_head">
-        <input type="submit" value="送出">
-    </form>
-    <button >
-    """
-'''
-
-@app.route('/')
-def index():
-    form = """
-    <form method="post" action="/printAllCourse" >
-        <button type="submit" name="AllCourse" value="*">Click Me</button>
-    </form>
-    <form method="post" action="/action2">
-        <button type="submit" name="AllCourse" value="CourseID, CourseName">Click Me 2</button>
-    </form>
-    """
-    return form
+# Written by Franco 2022/04/14
 
 
-@app.route('/printAllCourse', methods=['POST'])
-def printAllCourse():
-    # 取得輸入的文字
-    my_head = request.form.get("AllCourse")
-    #your_head = request.form.get("CourseName")
-    # 建立資料庫連線
-    conn = MySQLdb.connect(host="127.0.0.1",
-                           user="hj",
-                           passwd="test1234",
-                           db="testdb")
-    # 欲查詢的 query 指令
-    query1 = "SELECT {} FROM AllCourse;".format(my_head)
-    #query2 = "SELECT {} FROM AllCourse;".format(your_head)
-    # 執行查詢
-    cursor = conn.cursor()
-    cursor.execute(query1)
+from unittest import result
 
-    results = """
-    <p><a href="/">Back to Query Interface</a></p>
-    """
-    results = "<table>"
-    # 取得並列出所有查詢結果
-    #CourseID,CourseName,Dept,PeopleLimit,Points,Teacher,Grade,MustHave
-    results += "<tr>"
-    results += "<th>CourseID</th>, <th>CourseName</th>, <th>Dept</th>, <th>HowManyPeople</th>, <th>PeopleLimit</th>, <th>Points</th>, <th>Teacher</th>, <th>Grade</th>, <th>MustHave</th>"
-    results += "</tr>"
-    for (CourseID,CourseName,Dept,HowManyPeople, PeopleLimit,Points,Teacher,Grade,MustHav) in cursor.fetchall():
-        results += "<tr>"
-        results += "<td>{}</td>, <td>{}</td>, <td>{}</td>, <td>{}</td>, <td>{}</td>, <td>{}</td>, <td>{}</td>, <td>{}</td>, <td>{}</td>".format(CourseID,CourseName,Dept,HowManyPeople,PeopleLimit,Points,Teacher,Grade,MustHav)
-        results += "</tr>"
-    results = </table>
+
+def addUser(NID, UserName, UserPassword, Dept, Grade):
+    return f"insert into Users values(\'{NID}\', \'{UserName}\', \'{UserPassword}\', \'{Dept}\', {Grade});"
+
+def MustHaveList(NID):
+    return f"select CourseID from AllCourse where MustHave = true and Dept in (select Dept from Users where NID = {NID});"
+
+#if theres thing in this table, then theres time collision
+def timeCollision(NID, CourseID):
+    result =  f"SELECT TimeID from CourseTime WHERE CourseID IN (SELECT CourseID FROM Chosen WHERE NID = \'{NID}\')"
+    result += f" and "
+    result += f"TimeID IN (SELECT TimeID FROM CourseTime WHERE CourseID = {CourseID});"
+    return result
+
+#not include time collision 未完成
+def chooseCourse(NID, CourseID):
+    '''
+    timeTable = timeCollision(NID, CourseID)
+    currentTimeOfCourse = f"SELECT TimeID FROM CourseTime WHERE CourseID = {CourseID}"
+    result = f"IF (NOT EXISTS(SELECT TimeID FROM {currentTimeOfCourse} INNER JOIN {timeTable} ON {currentTimeOfCourse}.TimeID = {timeTable}.TimeID))"
+    if 
+    '''
+    return f"insert into Chosen values(\'{NID}\', {CourseID});"
+
+#not include "detect if the course is in NID's Chosen list"
+def deleteCourse(NID, CourseID):
+    return f"delete from Chosen where CourseID = {CourseID} and NID = \'{NID}\';"
+
+#null table donotes that don't exist same CourseName
+def isSameNameCourse(CourseName):
+    return f"SELECT CourseName FROM AllCourse WHERE CourseName = {CourseName};"
+
+#null table donotes that don't exceed limit of student
+def isExceedLimitOfStudent(CourseID):
+    return f"SELECT HowManyPeople FROM AllCourse WHERE CourseID = {CourseID} and HowManyPeople >= PeopleLimit;"
+
+#lists all CourseName, CourseID, Point that don't exceed limit of Point
+def ListChosenCourse(NID):
+    #source: python_example.py
+    cursor.execute(f"SELECT sum(Point) FROM AllCourse WHERE CourseID in (SELECT COURSEID FROM Chosen WHERE NID = {NID});")
+    currentToatalPointsOfStudent = cursor.fetchall()
+    cursor.execute(f"SELECT CourseName, CourseID, Point FROM AllCourse WHERE CourseID NOT IN (SELECT CourseID FROM Chosen);")
+    notChosenList = cursor.fetchall()
+    for (CourseName, CourseID, Point) in notChosenList:
+        sum = currentToatalPointsOfStudent + Point
+        if 9 <= sum and sum <= 30:
+            results = (CourseName, CourseID, Point)
     return results
 
-@app.route('/action2', methods=['POST'])
-def action2():
-    # 取得輸入的文字
-    my_head = request.form.get("AllCourse")
-    #your_head = request.form.get("CourseName")
-    # 建立資料庫連線
-    conn = MySQLdb.connect(host="127.0.0.1",
-                           user="hj",
-                           passwd="test1234",
-                           db="testdb")
-    # 欲查詢的 query 指令
-    query1 = "SELECT {} FROM AllCourse;".format(my_head)
-    #query2 = "SELECT {} FROM AllCourse;".format(your_head)
-    # 執行查詢
-    cursor = conn.cursor()
-    cursor.execute(query1)
+def isHigherPointLimit(NID):
+    results = f"SELECT sum(Point) FROM AllCourse WHERE CourseID in (SELECT COURSEID FROM Chosen WHERE NID = {NID});"
+    #source: python_example.py
+    cursor.execute(results)
+    if 30 < cursor.fetchall():
+        return True
+    return False
 
-    results = """
-    <p><a href="/">Back to Query Interface</a></p>
-    """
-    # 取得並列出所有查詢結果
-    #CourseID,CourseName,Dept,PeopleLimit,Points,Teacher,Grade,MustHave
+def isLowerPointLimit(NID):
+    results = f"SELECT sum(Point) FROM AllCourse WHERE CourseID in (SELECT COURSEID FROM Chosen WHERE NID = {NID});"
+    #source: python_example.py
+        cursor.execute(results)
+        if cursor.fetchall() < 30:
+            return True
+        return False
 
-    for (CourseID,CourseName) in cursor.fetchall():
-        results += "<p>{}, {}</p>".format(CourseID,CourseName)
-    return results
+def isMustHaveCourse(CourseID):
+    results =  f"SELECT MustHave FROM AllCourse WHERE CourseID = {CourseID}"
+    #source: python_example.py
+    cursor.execute(results)
+    if cursor.fetchall() == True:
+        return True
+    return False
+
+#Test
+NID = 'D0915679'
+CourseID = 9527
+print(MustHaveList(NID))
