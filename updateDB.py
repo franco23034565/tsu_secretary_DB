ba70855aace1df9111d9631f57982efd3d818f13
Peruschi <96690411+Peruschi@users.noreply.github.com>
2022-04-15 16:21:52 +0800

    Add files via upload

--- updateDB.py
+++ updateDB.py
@@ -1,24 +1,89 @@
-# Written by Franco 2022/04/14
+#!/usr/bin/env python3
+# coding=utf-8
+# -*- coding: UTF-8 -*-
+from flask import Flask, request
+import MySQLdb
+
+app = Flask(__name__)
+'''
+    form = """
+    <form method="post" action="/action" >
+        文字輸出欄位：<input name="my_head">
+        <input type="submit" value="送出">
+    </form>
+    <button >
+    """
+'''
+
+@app.route('/')
+def index():
+    form = """
+    <form method="post" action="/printAllCourse" >
+        <button type="submit" name="AllCourse" value="*">Click Me</button>
+    </form>
+    <form method="post" action="/action2">
+        <button type="submit" name="AllCourse" value="CourseID, CourseName">Click Me 2</button>
+    </form>
+    """
+    return form
 
 
-def addUser(NID, UserName, UserPassword, Dept, Grade):
-    return f"insert into Users values(\'{NID}\', \'{UserName}\', \'{UserPassword}\', \'{Dept}\', {Grade});"
+@app.route('/printAllCourse', methods=['POST'])
+def printAllCourse():
+    # 取得輸入的文字
+    my_head = request.form.get("AllCourse")
+    #your_head = request.form.get("CourseName")
+    # 建立資料庫連線
+    conn = MySQLdb.connect(host="127.0.0.1",
+                           user="hj",
+                           passwd="test1234",
+                           db="testdb")
+    # 欲查詢的 query 指令
+    query1 = "SELECT {} FROM AllCourse;".format(my_head)
+    #query2 = "SELECT {} FROM AllCourse;".format(your_head)
+    # 執行查詢
+    cursor = conn.cursor()
+    cursor.execute(query1)
 
-def MustHaveList(NID):
-    return f"select CourseID from AllCourse where MustHave = true and Dept in (select Dept from Users where NID = {NID});"
+    results = """
+    <p><a href="/">Back to Query Interface</a></p>
+    """
+    results = "<table>"
+    # 取得並列出所有查詢結果
+    #CourseID,CourseName,Dept,PeopleLimit,Points,Teacher,Grade,MustHave
+    results += "<tr>"
+    results += "<th>CourseID</th>, <th>CourseName</th>, <th>Dept</th>, <th>HowManyPeople</th>, <th>PeopleLimit</th>, <th>Points</th>, <th>Teacher</th>, <th>Grade</th>, <th>MustHave</th>"
+    results += "</tr>"
+    for (CourseID,CourseName,Dept,HowManyPeople, PeopleLimit,Points,Teacher,Grade,MustHav) in cursor.fetchall():
+        results += "<tr>"
+        results += "<td>{}</td>, <td>{}</td>, <td>{}</td>, <td>{}</td>, <td>{}</td>, <td>{}</td>, <td>{}</td>, <td>{}</td>, <td>{}</td>".format(CourseID,CourseName,Dept,HowManyPeople,PeopleLimit,Points,Teacher,Grade,MustHav)
+        results += "</tr>"
+    results = </table>
+    return results
 
-#if theres thing in this table, then theres time collision
-def timeCollision(NID, CourseID):
-    result =  f"SELECT TimeID from CourseTime WHERE CourseID IN (SELECT CourseID FROM Chosen WHERE NID = \'{NID}\')"
-    result += f" and "
-    result += f"TimeID IN (SELECT TimeID FROM CourseTime WHERE CourseID = {CourseID});"
-    return result
+@app.route('/action2', methods=['POST'])
+def action2():
+    # 取得輸入的文字
+    my_head = request.form.get("AllCourse")
+    #your_head = request.form.get("CourseName")
+    # 建立資料庫連線
+    conn = MySQLdb.connect(host="127.0.0.1",
+                           user="hj",
+                           passwd="test1234",
+                           db="testdb")
+    # 欲查詢的 query 指令
+    query1 = "SELECT {} FROM AllCourse;".format(my_head)
+    #query2 = "SELECT {} FROM AllCourse;".format(your_head)
+    # 執行查詢
+    cursor = conn.cursor()
+    cursor.execute(query1)
 
-#not include time collision
-def chooseCourse(NID, CourseID):
-    return f"insert into Chosen values(\'{NID}\', {CourseID});"
+    results = """
+    <p><a href="/">Back to Query Interface</a></p>
+    """
+    # 取得並列出所有查詢結果
+    #CourseID,CourseName,Dept,PeopleLimit,Points,Teacher,Grade,MustHave
 
-#not include "detect if the course is in NID's Chosen list"
-def deleteCourse(NID, CourseID):
-    return f"delete from Chosen where CourseID = {CourseID} and NID = \'{NID}\';"
-
+    for (CourseID,CourseName) in cursor.fetchall():
+        results += "<p>{}, {}</p>".format(CourseID,CourseName)
+    return results
