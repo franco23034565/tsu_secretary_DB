165cdfb6b7bda272f4f31ddd36501c44cde09ff3
Franco23034565 <franco23034565@gmail.com>
2022-04-21 14:04:40 +0800

    deleteCourse Able to use

--- UDBTest.py
+++ UDBTest.py
@@ -7,7 +7,7 @@ conn = MySQLdb.connect(host="127.0.0.1",
                            user="hj",
                            passwd="test1234",
                            db="testdb")
-
+cursor = conn.cursor()
 
 NID = "D0915679"
 userName = "紀彥廷"
@@ -15,4 +15,6 @@ userDept = "資訊工程學系"
 userPasswd = "IWasTesting"
 userGrade = 2
 # udb.addUser(NID, userName, userPasswd, userDept, userGrade, conn)
-udb.autoChooseMustHaveList(NID, conn)
+#udb.autoChooseMustHaveList(NID, conn)
+udb.deleteCourse(NID, 9487, conn)
+
